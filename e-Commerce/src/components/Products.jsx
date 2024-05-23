00e78bf512f576be25e1f5a7da8476f6ffaa5e5f
pankaj <pankajsbisht@88gmail.com>
2024-05-23 12:31:02 +0530

    pushed

--- e-Commerce/src/components/Products.jsx
+++ e-Commerce/src/components/Products.jsx
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../features/CartSlice';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 function Products() {
   const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
+  const indexLastProduct = currentPage * itemsPerPage;
+  const indexFirstProduct = indexLastProduct - itemsPerPage;
+  const currentProducts = products.slice(indexFirstProduct, indexLastProduct);
+  const totalPages = Math.ceil(products.length / itemsPerPage);
 
   useEffect(() => {
     const fetchProduct = async () => {
       const response = await axios.get('https://fakestoreapi.com/products');
       const data = response.data;
       const productFilter = data.filter(product => product.image !== null);
-      setProducts(productFilter.slice(0, 8));
+      setProducts(productFilter);
     };
     fetchProduct();
   }, []);
@@ -22,11 +29,23 @@ function Products() {
     dispatch(addToCart(product));
   };
 
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div className="container mx-auto bg-gray-100 py-8">
       <h1 className="text-3xl text-center font-bold mb-4">Products</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-1">
-        {products.map((product) => (
+        {currentProducts.map((product) => (
           <motion.div
             key={product.id}
             className="bg-white shadow-lg p-4 rounded-lg flex flex-col"
@@ -46,7 +65,7 @@ function Products() {
             <p className="text-gray-900 font-bold mb-4">${product.price}</p>
             <motion.button
               className="mt-auto bg-gray-700 text-white py-2 px-4 rounded"
-              whileHover={{ scale: 1.1, }}
+              whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9, backgroundColor: '#3B0D0C' }}
               onClick={() => handleAddToCart(product)}
             >
@@ -54,6 +73,26 @@ function Products() {
             </motion.button>
           </motion.div>
         ))}
+      </div>
+      <br />
+      <div className="flex justify-between bottom-0 mx-4">
+        <button
+          className={`mt-auto py-2 px-4 bottom-0 rounded ${currentPage === 1 ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-gray-700 text-white'}`}
+          onClick={handlePrev}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          className={`mt-auto py-2 px-4 bottom-0 rounded ${currentPage === totalPages ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-gray-700 text-white'}`}
+          onClick={handleNext}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
